c0a16152158a511ccca0b81d270a78c119f413fc
Mamta Shah <mamtashah1312@gmail.com>
2019-11-30 13:18:16 -0800

    updated k-ary heap to store node pointers

--- K-ary Heap/karyHeap.cpp
+++ K-ary Heap/karyHeap.cpp
@@ -3,9 +3,14 @@
 #include <vector>
 using namespace std;
 
+struct node{
+	int vertex;
+	int weight;
+};
+
 class karyHeap{
 private: 
-	vector<int> heap;
+	vector<node*> heap;
 	int num_elems = 0;
 	// Shifts the last element of the heap to it's correct position
 	void siftUp(int k){
@@ -14,8 +19,8 @@ private:
 		while(index > 0){
 			int parent = floor((index -1)/k);
 
-			if(heap[parent] > heap[index]){
-				int temp = heap[parent];
+			if(heap[parent]->weight > heap[index]->weight){
+				node* temp = heap[parent];
 				heap[parent] = heap[index];
 				heap[index] = temp;
 				index = parent;
@@ -43,18 +48,18 @@ private:
 			}
 			
 			// find minimum of the k childrens of parent at index 
-			int min = heap[startChild];
+			int min = heap[startChild]->weight;
 			childIndex = startChild;
 			for(int child = startChild+1; child <= endChild; child++ ){
-				if(min > heap[child]){
-					min = heap[child];
+				if(min > heap[child]->weight){
+					min = heap[child]->weight;
 					childIndex = child;
 				}
 			}
 		
 			// swap the min valued child with the parent
-			if(heap[index] > heap[childIndex]){
-				int temp = heap[index];
+			if(heap[index]->weight > heap[childIndex]->weight){
+				node* temp = heap[index];
 				heap[index] = heap[childIndex];
 				heap[childIndex] = temp;
 
@@ -74,8 +79,8 @@ public:
 	}
 
 	// for inserting just add the element to the end of heap and then shift it up to the correct position
-	void insertIntoHeap(int k, int val){
-		heap.push_back(val);
+	void insertIntoHeap(int k, node* n1){
+		heap.push_back(n1);
 
 		num_elems++;
 
@@ -84,25 +89,25 @@ public:
 
 	// for getting the Min remove the root and place last element at its position and then shift it down
 	// to the appropriate position
-	int getMin(int k){
+	node* getMin(int k){
 		if(num_elems <= 0)
-			return -1;
+			return NULL;
 
-		int result = heap[0];
+		node* result = heap[0];
 		num_elems -= 1;
 		heap[0] = heap[num_elems];
 		
 		siftDown(k);
 
-		heap[num_elems] = -1;
+		heap[num_elems] = NULL;
 
 		return result;
 	}
 
 	// Show the contents of the heap at this point 
 	void showHeap(){
-		for(int j =0; j < heap.size(); j++){
-			printf("%d\t", heap[j]);
+		for(int j =0; j < num_elems; j++){
+			printf("(%d, %d)\t", heap[j]->vertex, heap[j]->weight);
 		}
 		printf("\n");
 	}
@@ -113,28 +118,31 @@ public:
 /*int main(){
 
 	karyHeap obj;
+	int ary = 3;
 
-	
-	obj.insertIntoHeap(3, 3);
-	obj.insertIntoHeap(3, 5);
-	obj.insertIntoHeap(3, 8);
-	obj.insertIntoHeap(3, 1);
+	node* x = (node*) malloc(sizeof(node*));
+	x->vertex = 0;
+	x->weight = 3;
+	obj.insertIntoHeap(ary, x);
+	node* x1 = (node*) malloc(sizeof(node*));
+	x1->vertex = 1;
+	x1->weight = 5;
+	obj.insertIntoHeap(ary, x1);
+	node* x2 = (node*) malloc(sizeof(node*));
+	x2->vertex = 2;
+	x2->weight = 8;
+	obj.insertIntoHeap(ary, x2);
+	node* x3 = (node*) malloc(sizeof(node*));
+	x3->vertex = 3;
+	x3->weight = 1;
+	obj.insertIntoHeap(ary, x3);
 
 	obj.showHeap();
 
-	cout << obj.getMin(3) << endl;
+	cout << obj.getMin(ary)->weight << endl;
 	obj.showHeap();
 
-	cout << obj.getMin(3) << endl;
-	obj.showHeap();
-
-	cout << obj.getMin(3) << endl;
-	obj.showHeap();
-
-	cout << obj.getMin(3) << endl;
-	obj.showHeap();
-
-	cout << obj.getMin(3) << endl;
+	cout << obj.getMin(ary)->weight << endl;
 	obj.showHeap();
 
 	return 0;
